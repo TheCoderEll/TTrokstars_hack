7e024bb7a471b98326eb1a74bb9c3649d2466d98
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-05-13 19:33:18 +0100

    Update Proc.js

--- Proc.js
+++ Proc.js
@@ -2,7 +2,7 @@ function initilise_button(){
 	var opened = window.open("");
 	opened.document.write("<button onclick='ans()'>ANS</button>");
 }
-function init(){
+function proc(){
 	initilise_button()
 }
-init()
+proc()
