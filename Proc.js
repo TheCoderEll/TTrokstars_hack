2329e844439cab8fd253df68135c40a09bb4ad2d
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-05-13 19:41:01 +0100

    Update Proc.js

--- Proc.js
+++ Proc.js
@@ -2,6 +2,6 @@ function initilise_button(){
 	function () { var script = document.createElement('button'); script.innerHTML = "ANS"; script.onclick = "ans()"; document.body.appendChild(script);}();
 }
 function proc(){
-	initilise_button()
+	initilise_button();
 }
-proc()
+proc();
