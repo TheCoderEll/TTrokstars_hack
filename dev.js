73860a3a3f2f1803f82c01c4a95667a1cc421bd1
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-05-15 12:42:05 +0100

    Update dev.js

--- dev.js
+++ dev.js
@@ -4,19 +4,32 @@ function ans(){
 	no1 = text[0];
 	op = text[1];
 	no2 = text[2];
+	answer = "";
 	if (op == "×") {
-		
-function createKeyboardEvent(name, key, altKey, ctrlKey, shiftKey, metaKey, bubbles) {
-  var e = new Event(name)
-  e.key = key
-  e.keyCode = e.key.charCodeAt(0)
-  e.which = e.keyCode
-  e.altKey = altKey
-  e.ctrlKey = ctrlKey
-  e.shiftKey = shiftKey
-  e.metaKey =  metaKey
-  e.bubbles = bubbles
-  return e
+		answer = no1 * no2
+	}
+	if (op == "÷") {
+		anser = no1 / no2
+	}
+	nol = anser.split('');
+	if (nol.length == 1){
+		simulateKeydown (no1[0].keycode, false, false, false);
+	}
+	if (nol.length == 2){
+		simulateKeydown (no1[0].keycode, false, false, false);
+		simulateKeydown (no1[1].keycode, false, false, false);
+	}
+	if (nol.length == 3){
+		simulateKeydown (no1[0].keycode, false, false, false);
+		simulateKeydown (no1[1].keycode, false, false, false);
+		simulateKeydown (no1[2].keycode, false, false, false);
+	}
+	simulateKeydown (13, false, false, false);
+function simulateKeydown (keycode,isCtrl,isAlt,isShift){
+	var e = new KeyboardEvent( "keydown", { bubbles:true, cancelable:true, char:String.fromCharCode(keycode), key:String.fromCharCode(keycode), shiftKey:isShift, ctrlKey:isCtrl, altKey:isAlt } );
+        Object.defineProperty(e, 'keyCode', {get : function() { return this.keyCodeVal; } });     
+        e.keyCodeVal = keycode;
+        document.dispatchEvent(e);
 }
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
