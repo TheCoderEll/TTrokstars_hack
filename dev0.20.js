b35d34290607dc6c4be89f0af61c9502303d1e4a
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-05-16 17:52:54 +0100

    Update and rename dev0.18.js to dev0.20.js

--- dev0.20.js
+++ dev0.20.js
@@ -4,7 +4,7 @@ function ans(){
 	text = q.split(" ");
 	alert(text);
 	no1 = text[1];
-	op = text[3].split("<!---->")[1];
+	op = text[2].split("<!---->")[1];
 	no2 = text[4];
 	alert(no1);
 	alert(op);
