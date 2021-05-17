faa36e30074f9289f68a33cc845fa955de42e66d
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-05-17 11:50:08 +0100

    Update and rename dev5.07.js to dev5.08.js

--- dev5.08.js
+++ dev5.08.js
@@ -38,7 +38,7 @@ function ans(){
 		simulateKeydown (48 + parseInt(nol[1]), false, false, false);
 		simulateKeydown (48 + parseInt(nol[2]), false, false, false);
 	}
-	//simulateKeydown (13, false, false, false);
+	simulateKeydown (13, false, false, false);
 }
 function simulateKeydown (keycode,isCtrl,isAlt,isShift){
 	var e = new KeyboardEvent( "keydown", { bubbles:true, cancelable:true, char:String.fromCharCode(keycode), key:String.fromCharCode(keycode), shiftKey:isShift, ctrlKey:isCtrl, altKey:isAlt } );
